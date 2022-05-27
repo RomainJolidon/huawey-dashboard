3734e6bf495ad9aa992af77c5ead1bbef397b017
Clément <61332243+xklement@users.noreply.github.com>
2022-05-27 21:20:24 +0200

    [ADD] beginning of the frontend and a custom Navigation Bar

---
+++ Mobile/entry/src/main/js/default/pages/index/weather/weather.js
@@ -0,0 +1,10 @@
+export default {
+    data: {
+        title: 'World',
+        pageIndex: 1
+    },
+    onInit(){
+        console.info(this.data.pageIndex);
+        console.log(this.data.pageIndex);
+    }
+}
