62bda523d0dc82a2d2a708648dec010ddb709b34
Clément <61332243+xklement@users.noreply.github.com>
2022-05-28 16:24:53 +0200

    [FIX] navBar logo path

--- Mobile/entry/src/main/js/default/pages/index/weather/weather.js
+++ Mobile/entry/src/main/js/default/pages/index/weather/weather.js
@@ -1,7 +1,12 @@
 export default {
     data: {
         title: 'World',
-        pageIndex: 1
+        pageIndex: 1,
+        fakeData : [
+        {"text":"Profile","img":'./common/images/user.png', "route": 'pages/index/default/default'},
+        {"text":"Weather","img":"./common/images/cloudy.png", "route": 'pages/index/weather/weather'},
+        {"text":"News","img":"./common/images/news-report.png", "route": 'pages/index/news/news'},
+        ]
     },
     onInit(){
         console.info(this.data.pageIndex);
