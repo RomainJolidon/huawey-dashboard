f074f5e9e51028fd2f5c1c122260189a5c04b877
Clément <61332243+xklement@users.noreply.github.com>
2022-06-07 12:50:53 +0200

    [ADD] re testing the create user route: working with the status code

--- Mobile/entry/src/main/js/default/pages/index/index.js
+++ Mobile/entry/src/main/js/default/pages/index/index.js
@@ -1,24 +1,67 @@
 import router from '@system.router';
+import fetch from '@system.fetch';
 export default {
+    data: {
+        loginName: "",
+        loginPassword: "",
+
+    },
+
     onHide() {
         this.closePanel();
     },
+
     launch() {
-        router.push ({
-            uri: 'pages/index/default/default',
-            params: {
-                data:{pageIndex : 0}
+
+        const dataFromUser = {
+            "name": this.loginName,
+            "password": this.loginPassword
+        };
+        fetch.fetch({
+            url:'https://d559-88-166-52-147.ngrok.io/user/login',
+            data: dataFromUser,
+            method:'POST',
+            header:{
+                'content-type':'application/json'
+            },
+            responseType:'json',
+            success(response){
+                console.log(typeof response.data)
+                console.log('getListData fetch success:' + JSON.stringify(response))
+
+            },
+            fail(data,code){
+                if (data.code == 200)
+                {
+                    console.log('create user');
+                    console.log('getListData fetch success:' + JSON.stringify(code) + JSON.stringify(data))
+                    /*router.push ({
+                        uri: 'pages/index/default/default',
+                        params: {
+                            data:{pageIndex : 0}
+                        }
+                    });*/
+                }
+                console.log('getListData fetch fail:' + JSON.stringify(code) + JSON.stringify(data))
+            },
+            complete(...args){
+                console.log('getListData fetch complete:' + JSON.stringify(args))
             }
         });
-        console.log("here");
     },
+
     showPanel() {
         this.$element('simplepanel').show()
     },
+
     closePanel() {
         this.$element('simplepanel').close()
+        console.log(this.data.value);
     },
-    changeMode(e) {
-        this.modeFlag = e.mode
-    }
+    getLoginName(e) {
+        this.loginName = e.value;
+    },
+    getLoginPassword(e) {
+        this.loginPassword = e.value;
+    },
 }
--- Mobile/entry/src/main/js/default/pages/index/signUP/signUP.js
+++ Mobile/entry/src/main/js/default/pages/index/signUP/signUP.js
@@ -1,15 +1,61 @@
 import router from '@system.router';
+import fetch from '@system.fetch';
+
 export default {
     data: {
-        title: 'World',
+        RegisterName: "",
+        RegisterPassword: "",
+        RegisterEmail: "",
     },
-    launch() {
-        router.push ({
-            uri: 'pages/index/default/default',
-            params: {
-                data:{pageIndex : 0}
+
+    register() {
+        const dataFromUser = {
+            "name": this.RegisterName,
+            "email": this.RegisterEmail,
+            "password": this.RegisterPassword
+        };
+
+        fetch.fetch({
+            url:'https://d559-88-166-52-147.ngrok.io/user/register',
+            data: dataFromUser,
+            method:'POST',
+            header:{
+                'content-type':'application/json'
+            },
+            responseType:'json',
+            success(response){
+                console.log(typeof response.data)
+                console.log('getListData fetch success:' + JSON.stringify(response))
+
+            },
+            fail(data,code){
+                if (data.code == 201)
+                {
+                    console.log('create user');
+                    console.log('getListData fetch success:' + JSON.stringify(code) + JSON.stringify(data))
+                    /*router.push ({
+                        uri: 'pages/index/default/default',
+                        params: {
+                            data:{pageIndex : 0}
+                        }
+                    });*/
+                }
+                console.log('getListData fetch fail:' + JSON.stringify(code) + JSON.stringify(data))
+            },
+            complete(...args){
+                console.log('getListData fetch complete:' + JSON.stringify(args))
             }
         });
-        console.log("here");
+    },
+
+    getRegisterName(e) {
+        this.RegisterName = e.value;
+        console.info(this.RegisterName);
+    },
+    getRegisterPassword(e) {
+        this.RegisterPassword = e.value;
+    },
+    getRegisterEmail(e) {
+        this.RegisterEmail = e.value;
     },
 }
